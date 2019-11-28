bdc836ac0ad1d39b5b41e84ff0a2e0261b1a8a7d
nick270800 <58128521+nick270800@users.noreply.github.com>
2019-11-28 00:19:52 -0300

    Update Trab.java

--- Trab.java
+++ Trab.java
@@ -91,3 +91,88 @@ public class GerenciaBib{       //Gerenciador da Biblioteca (Atividade)
                 this.CPF = CPF;
             }
         }
+
+            public class Usuario{
+            private String Nome;
+            private int RG;
+            private int Chapa;
+            private int CPF;
+
+            public String getNome (){
+                return this.Nome;
+            }
+
+            public void setNome(String Nome){
+                this.Nome = Nome;
+            }
+
+            public int getRG(){
+                return this.RG;
+            }
+
+            public void setRG(int RG){
+                this.RG = RG;
+            }
+
+            public int getChapa(){
+                return this.Chapa;
+            }
+
+            public void setChapa(int Chapa){
+                this.Chapa = Chapa;
+            }
+
+            public int getCPF(){
+                return this.CPF;
+            }
+
+            public void setCPF(int CPF){
+                this.CPF = CPF;
+            }
+       }
+
+        public abstract class Midia {
+           private String NomeLivro;
+           private String NomeDVD;
+           private String Autor;
+           private String Genero;
+
+           public String getNomeLivro(){
+               return this.NomeLivro;
+           }
+
+           public void setNomeLivro(String NomeLivro){
+               this.NomeLivro = NomeLivro;
+           }
+
+           public String getNomeDVD(){
+               return this.NomeDVD;
+           }
+
+           public void setNomeDVD(String NomeDVD){
+               this.NomeDVD = NomeDVD;
+           }
+
+           public String getAutor(){
+               return this.Autor;
+           }
+
+           public void setAutor(String Autor){
+               this.Autor = Autor;
+           }
+
+           public String getGenero(){
+               return this.Genero;
+           }
+
+           public void setGenero(String Genero){
+               this.Genero = Genero;
+           }
+       
+           public Midia (String NomeLivro, String NomeDVD, String Autor, String Genero) {
+               this.NomeDVD = null;
+               this.NomeLivro = null;
+               this.Autor = null;
+               this.Genero = null;
+           }
+        }
